96da9b91da7270feeb52025b7cf0e1f89950c760
qiyisi <qiyisi@qq.com>
2022-01-29 16:40:39 +0800

    Update

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,75 +1,100 @@
 import styled from "styled-components";
 
+import Container from "components/container";
+import ExternalLink from "components/externalLink";
+
 const Wrapper = styled.div`
-  display: flex;
+  border-top: 1px solid #ebeef4;
+  background: #ffffff;
+  position: relative;
+`;
+
+const TopWrapper = styled.div`
   height: 64px;
-  align-items: center;
-  font-size: 14px;
-  font-weight: 500;
-  color: #506176;
-
-  .mr-16 {
-    margin-right: 16px;
-  }
-
-  .mr-8 {
-    margin-right: 8px;
-  }
-`
-
-const Contents = styled.div`
-  margin: auto;
-  width: 100%;
-  max-width: 1080px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
-`
+  > :first-child {
+    display: flex;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 100%;
+    color: #506176;
+    > :not(:first-child) {
+      margin-left: 24px;
+    }
+  }
+  > :last-child {
+    display: flex;
+    > :not(:first-child) {
+      margin-left: 8px;
+    }
+  }
+`;
 
-const Section = styled.div`
+const BottomWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   display: flex;
   align-items: center;
-  width: 33.33%;
-
-  .light {
-    color: #9DA9BB;
+  > :first-child {
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 100%;
+    color: #506176;
   }
-`
-
-const SectionRight = styled(Section)`
-  justify-content: end;
-`
-
-const Link = styled.a`
-  margin-right: 24px;
-`
+  > :last-child {
+    margin-left: 16px;
+    display: flex;
+    align-items: center;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 100%;
+    color: #9da9bb;
+    > :not(:first-child) {
+      margin-left: 16px;
+    }
+  }
+  @media screen and (max-width: 800px) {
+    position: static;
+    padding: 16px 0;
+    transform: none;
+    flex-direction: column;
+    > :last-child {
+      margin-left: 0;
+      margin-top: 16px;
+    }
+  }
+`;
 
 export default function Footer() {
-  return (<Wrapper>
-    <Contents>
-      <Section>
-        <Link href="/terms">
-          Terms
-        </Link>
-        <Link href="privacy">
-          Privacy
-        </Link>
-      </Section>
-
-      <Section>
-        <span className="mr-16">© 2022 SubSquare</span>
-        <span className="mr-16 light">Powered by</span>
-        <img src="/imgs/logo-grey.svg" alt=""/>
-      </Section>
-
-      <SectionRight>
-        {/*todo: add link for this*/}
-        <Link className="mr-8">
-          <img src="/imgs/icons/element.svg" alt=""/>
-        </Link>
-        <Link href="mailto:hi@opensquare.network">
-          <img src="/imgs/icons/mail.svg" alt=""/>
-        </Link>
-      </SectionRight>
-    </Contents>
-  </Wrapper>);
+  return (
+    <Wrapper>
+      <Container>
+        <TopWrapper>
+          <div>
+            <ExternalLink href="/">Terms</ExternalLink>
+            <ExternalLink href="/">Privacy</ExternalLink>
+          </div>
+          <div>
+            <ExternalLink href="mailto:hi@opensquare.network">
+              <img src="/imgs/icons/mail.svg" alt="" />
+            </ExternalLink>
+            <ExternalLink href="https://app.element.io/#/room/#opensquare:matrix.org">
+              <img src="/imgs/icons/element.svg" alt="" />
+            </ExternalLink>
+          </div>
+        </TopWrapper>
+        <BottomWrapper>
+          <div>© 2022 SubSquare</div>
+          <div>
+            <div>Powered by</div>
+            <img src="/imgs/logo-opensquare.svg" alt="" />
+          </div>
+        </BottomWrapper>
+      </Container>
+    </Wrapper>
+  );
 }
