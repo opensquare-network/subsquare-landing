e730e51c294ac507dd40273998184c3a88b1ca9a
qiyisi <qiyisi@qq.com>
2022-02-11 16:46:05 +0800

    Add header shadow, #15

--- components/header/index.js
+++ components/header/index.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Container from "components/container";
 import Launch from "./launch";
 import Sider from "./sider";
+import { useScrollPosition } from "utils/hooks";
 
 const Wrapper = styled.header`
   z-index: 2;
@@ -11,6 +12,11 @@ const Wrapper = styled.header`
   height: 64px;
   position: fixed;
   width: 100%;
+  &.shadow {
+    box-shadow: 0px 6px 7px rgba(30, 33, 52, 0.02),
+      0px 1.34018px 1.56354px rgba(30, 33, 52, 0.0119221),
+      0px 0.399006px 0.465507px rgba(30, 33, 52, 0.00807786);
+  }
 `;
 
 const InnerWrapper = styled.div`
@@ -26,8 +32,10 @@ const Logo = styled.div`
 `;
 
 export default function Header() {
+  const position = useScrollPosition();
+
   return (
-    <Wrapper>
+    <Wrapper className={position > 0 ? "shadow" : ""}>
       <Container>
         <InnerWrapper>
           <Logo />
