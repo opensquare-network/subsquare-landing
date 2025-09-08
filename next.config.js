04512b419cbac0ce0d068a2ecdfacd43cc588c27
quinn-gao <quinnn.gao@gmail.com>
2025-09-08 14:53:55 +0800

    feat: add custom headers for enhanced security

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          // disabled iframe
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'none';",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+        ],
+      },
+    ];
+  },
+};
